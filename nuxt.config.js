523f4bfe4ca37f050001a6010374eef0d64c9e35
Timur Brachkow <brachkow@fastmail.com>
2021-02-16 16:50:24 +0300

    Tailwind added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   target: 'static',
   modules: ['@nuxt/content'],
+  buildModules: ['@nuxtjs/tailwindcss'],
   plugins: [{ src: '~/plugins/v-clipboard.js', ssr: false }],
   generate: {
     async routes() {
